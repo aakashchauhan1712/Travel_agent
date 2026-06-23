cc9503c5fbd5608d16d3aea449d7cbcf4383086f
aakashchauhan1712 <aakashchauhan1712@gmail.com>
2026-06-24 01:24:02 +0530

    v0.6.1 improved RAG pipeline

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from dotenv import load_dotenv
 
@@ -6,19 +7,24 @@ from langchain_community.vectorstores import FAISS
 
 load_dotenv()
 
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+vector_store = FAISS.load_local(
+    "faiss_index",
+    embeddings,
+    allow_dangerous_deserialization=True
+)
 
 def retrieve_context(query):
-
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",
-        google_api_key=os.getenv("GOOGLE_API_KEY")
-    )
-
-    vector_store = FAISS.load_local(
-        "faiss_index",
-        embeddings,
-        allow_dangerous_deserialization=True
-    )
 
     docs = vector_store.similarity_search(
         query,
